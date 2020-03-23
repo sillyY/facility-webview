94c8c5a6a095a6f0f42f56ab2bb78086ecac3464
sillyy <965768042@qq.com>
2020-03-23 09:57:14 +0800

    feat 调整config cdn

--- src/services/remote.js
+++ src/services/remote.js
@@ -1,5 +1,5 @@
 export const remote = {
   url: "https://cdn.jsdelivr.net/gh/sillyY/template-library",
-  config_url: "https://cdn.jsdelivr.net/gh/sillyY/template-library@0.0.1/config.json",
+  config_url: "https://cdn.jsdelivr.net/gh/sillyY/template-library/config.json",
   source_url: "https://cdn.jsdelivr.net/gh/sillyY/template-library/sources/"
 };
